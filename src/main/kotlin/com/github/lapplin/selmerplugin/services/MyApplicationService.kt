1d59bcba68b357d6154b64822702dadd10d94511
GitHub Action <action@github.com>
2021-07-18 08:33:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/lapplin/selmerplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lapplin/selmerplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lapplin.selmerplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lapplin.selmerplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/lapplin/selmerplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lapplin/selmerplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lapplin.selmerplugin.services
 
+import com.github.lapplin.selmerplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
